db5ee23aaa3de311cac6f574c5f234a078ea7b86
Blayne Marjama <blayne.marjama@gmail.com>
2017-04-23 11:38:41 -0700

    added some comments to explain

--- src/components/menu.js
+++ src/components/menu.js
@@ -8,9 +8,13 @@ import Items from '../data/items';
 
 
 class Menu extends Component {
+
+	// this will go through Data/items and return the items as butons able to be clicked.
 	renderButtons(toRender){
 		return toRender.map((item, index) => {
+			// if Data/items/price has more than one price i.e. has a small/large price, it will return buttons for each
 			if (Object.keys(item.price).length > 1) {
+				// returns the size and price of the brands item
 				return Object.keys(item.price).map((size, i) => {
 					return(
 						<button key={item.brand + item.price[size] + i}>
@@ -20,6 +24,7 @@ class Menu extends Component {
 					)
 				})
 			} else {
+				// if it doesnt have multiple prices, will return correctly
 				return(
 						<button key={item.brand + item.price + index}>
 							<div>{item.brand}</div>
@@ -31,6 +36,9 @@ class Menu extends Component {
 	}
 
 	render() {
+		// gets rid of needing to do 'Items.alcohol.beer.tap', simple cleanup
+		const { beer, liquor } = Items.alcohol
+
 		return (
 			<div className="container">
 				<div className="tab-content">
@@ -41,19 +49,19 @@ class Menu extends Component {
 						<h2>Alcohol</h2>
 						<div>
 							<h4>tap</h4>
-							{this.renderButtons(Items.alcohol.beer.tap)}
+							{this.renderButtons(beer.tap)}
 						</div>
 						<div>
 							<h4>bottle</h4>
-							{this.renderButtons(Items.alcohol.beer.bottle)}
+							{this.renderButtons(beer.bottle)}
 						</div>
 						<div>
 							<h4>vodka</h4>
-							{this.renderButtons(Items.alcohol.liquor.vodka)}
+							{this.renderButtons(liquor.vodka)}
 						</div>
 						<div>
 							<h4>tequila</h4>
-							{this.renderButtons(Items.alcohol.liquor.tequila)}
+							{this.renderButtons(liquor.tequila)}
 						</div>
 					</div>
 					<div id="food" className="tab-pane fade">
